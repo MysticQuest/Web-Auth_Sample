6d921849676afa184584c825328f43b9f3847497
Mystic Quest <l1skou@gmail.com>
2019-11-03 01:58:54 +0200

    added DB & login/register functions

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -12,6 +13,29 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 app.use(express.static("public"));
+
+//----------------------DB SETUP-------------------------
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+};
+
+mongoose.connect("mongodb://localhost:27017/userAuthDB", options);
+
+const userSchema = {
+  email: {
+    type: String,
+    required: [true, "Error - Blank Entry"]
+  },
+  password: {
+    type: String,
+    required: [true, "Error - Blank Entry"]
+  }
+};
+
+const User = mongoose.model("User", userSchema);
+
 
 
 //---------------------ROUTING---------------------------
@@ -28,7 +52,39 @@ app.get("/register", function(req, res) {
   res.render("register");
 });
 
+app.post("/register", function(req, res) {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
 
+  newUser.save(function(err) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("secrets");
+    }
+  });
+});
+
+app.post("/login", function(req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({
+    email: username
+  }, function(err, foundUser) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        if (foundUser.password === password) {
+          res.render("secrets");
+        }
+      }
+    }
+  });
+});
 
 
 
