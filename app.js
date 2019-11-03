f117b390b7ccefc6708a3c6f59b964362666fbd9
Mystic Quest <l1skou@gmail.com>
2019-11-03 03:34:42 +0200

    added hashing

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const encrypt = require("mongoose-encryption");
+const md5 = require('md5');
+//const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -38,10 +39,10 @@ const userSchema = new mongoose.Schema({
   }
 });
 
-userSchema.plugin(encrypt, {
+/*userSchema.plugin(encrypt, {
   secret: process.env.SECRET,
   encryptedFields: ["password"]
-});
+});*/
 
 const User = mongoose.model("User", userSchema);
 
@@ -64,7 +65,7 @@ app.get("/register", function(req, res) {
 app.post("/register", function(req, res) {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
 
   newUser.save(function(err) {
@@ -78,7 +79,7 @@ app.post("/register", function(req, res) {
 
 app.post("/login", function(req, res) {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({
     email: username
